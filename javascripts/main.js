b6e6ab795a0bf62f9a0e58003762198fe2918730
Sai Pc <schand31@asu.edu>
2015-12-30 22:35:54 +0530

    Initial commit without ajaxed json data

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+
+var graph1 = {
+  name: ".projects-bar-chart-svg",
+  height: 500,
+  width: 1000
+};
+
+drawBarGraph(jsonData1, graph1);
+
+var graph2 = {
+  name: ".loc-bar-chart-svg",
+  height: 500,
+  width: 1000
+};
+
+drawBarGraph(jsonData2, graph2);
